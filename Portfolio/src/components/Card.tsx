7d8610fdd76f0c0910233e2b1a1d888b45fe2845
Alvarofr09 <alvarofriasruiz03@gmail.com>
2025-02-24 18:26:17 +0100

    Component structure

---
+++ Portfolio/src/components/Card.tsx
@@ -0,0 +1,20 @@
+export default function Card() {
+	return (
+		<div>
+			<header></header>
+			<div>
+				<h3>
+					<div></div>
+					<a href=""></a>
+				</h3>
+				<p></p>
+				<ul>
+					<li></li>
+				</ul>
+				<ul>
+					<li></li>
+				</ul>
+			</div>
+		</div>
+	);
+}
