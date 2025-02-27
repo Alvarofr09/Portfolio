4b8a77056019ed970b5a976f049e306ad7002bf3
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2025-02-27 19:40:19 +0100

    Change the base structure of the cards, and some minos changes of css, made the new div to be above the card

--- Portfolio/src/components/Card.tsx
+++ Portfolio/src/components/Card.tsx
@@ -27,33 +27,37 @@ const Card: React.FC<CardProps> = ({
 	links,
 }) => {
 	return (
-		<div className="mb-10 group grid pb-1 sm:grid-cols-8 sm:gap-8 md:gap-4">
-			<header className="sm:col-span-2">
-				{isProject ? (
-					<img src={image} alt="{enterprise}_image" />
-				) : (
-					<span className="uppercase">{duration}</span>
-				)}
-			</header>
-			<div className="sm:col-span-6">
-				<h3>
-					<div></div>
-					<a
-						href={isProject ? links.enterprise : links.github}
-						className="group-hover:text-red-600"
-					>
-						<span>
-							{role} · <span>{enterprise}</span>
-						</span>
-					</a>
-				</h3>
-				<p className="mt-2">{description}</p>
-				{/* <ul>
+		<li className="mb-12">
+			<div className="relative group grid pb-1 sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+				<div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-4 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+
+				<header className=" z-10 sm:col-span-2">
+					{isProject ? (
+						<img src={image} alt="{enterprise}_image" />
+					) : (
+						<span className="uppercase">{duration}</span>
+					)}
+				</header>
+				<div className="z-10 sm:col-span-6">
+					<h3 className="font-medium leading-snug text-texto-titulos">
+						<div></div>
+						<a
+							href={isProject ? links.enterprise : links.github}
+							className="group-hover:text-texto-enfasis"
+						>
+							<span>
+								{role} · <span>{enterprise}</span>
+							</span>
+						</a>
+					</h3>
+					<p className="mt-2 text-sm leading-normal">{description}</p>
+					{/* <ul>
 					<li></li>
 				</ul> */}
-				<Technologies technologies={technologies} />
+					<Technologies technologies={technologies} />
+				</div>
 			</div>
-		</div>
+		</li>
 	);
 };
 
