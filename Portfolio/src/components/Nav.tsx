7f74c9f7c426e58affedd8836d3bb8c5e1a5e10a
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2025-02-27 19:40:56 +0100

    Change to know in which part of the page we are and use of the new color variables

--- Portfolio/src/components/Nav.tsx
+++ Portfolio/src/components/Nav.tsx
@@ -7,20 +7,40 @@ interface NavItem {
 
 interface NavProps {
 	items: NavItem[];
+	activeSection: string;
 }
 
-const Nav: React.FC<NavProps> = ({ items }) => {
+const Nav: React.FC<NavProps> = ({ items, activeSection }) => {
+	const handleClick = (id: string) => {
+		const section = document.getElementById(id);
+		if (section) {
+			section.scrollIntoView({ behavior: "smooth" });
+		}
+	};
 	return (
 		<nav className="nav hidden lg:block">
 			<ul className="mt-16 w-max">
 				{items.map((item: NavItem, index: Key | null | undefined) => (
 					<li key={index}>
-						<a href={`#${item.id}`} className="group flex items-center py-3">
-							<span className="mr-4 h-0.5 w-8 bg-[#e5e7eb] group-hover:bg-white group-hover:w-16 transition-all"></span>
-							<span className="uppercase group-hover:text-white">
+						<button
+							onClick={() => handleClick(item.id)}
+							className={`group flex items-center py-3 ${
+								activeSection === item.id
+									? "text-texto-titulos"
+									: "text-texto-bg-texto-principal"
+							}`}
+						>
+							<span
+								className={`mr-4 h-0.5 w-8 bg-texto-principal group-hover:bg-texto-titulos text-texto-titulos group-hover:w-16 transition-all ${
+									activeSection === item.id
+										? "bg-texto-titulos text-texto-titulos w-16"
+										: "bg-texto-principal w-8"
+								}`}
+							></span>
+							<span className="uppercase group-hover:text-texto-titulos">
 								{item.title}
 							</span>
-						</a>
+						</button>
 					</li>
 				))}
 			</ul>
