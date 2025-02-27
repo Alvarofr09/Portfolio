317f6d48ac3e21078987dab56db13e5bfeeee557
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2025-02-27 19:38:43 +0100

    Create component Links

---
+++ Portfolio/src/components/Link.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface LinkProps {
+	href: string;
+	text: string;
+}
+
+const Link: React.FC<LinkProps> = ({ href, text }) => {
+	return (
+		<a
+			href={href}
+			className="font-medium text-texto-titulos hover:text-texto-enfasis focus-visible:text-texto-enfasis"
+			target="_blank"
+			rel="noopener noreferrer"
+			aria-label="{text} (New Tab)"
+		>
+			{text}
+		</a>
+	);
+};
+
+export default Link;
