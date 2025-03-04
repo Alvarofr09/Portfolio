cc44377a9f50c521f94f0b95386f9700ee109ae3
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2025-03-04 10:37:39 +0100

    Config of preferredLanguage

--- Portfolio/src/config/i18next.config.tsx
+++ Portfolio/src/config/i18next.config.tsx
@@ -2,11 +2,20 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 
+const getPreferredLanguage = () => {
+	const savedLanguage = localStorage.getItem("language");
+	if (savedLanguage) return savedLanguage;
+
+	const browserLanguage = navigator.language.split("-")[0];
+	return ["es", "en"].includes(browserLanguage) ? browserLanguage : "es";
+};
+
 i18n
 	.use(Backend)
 	.use(initReactI18next)
 	.init({
-		fallbackLng: "es",
+		lng: getPreferredLanguage(),
+		fallbackLng: getPreferredLanguage(),
 		// debug: true,
 		interpolation: {
 			escapeValue: false, // Desactiva el escape de caracteres
