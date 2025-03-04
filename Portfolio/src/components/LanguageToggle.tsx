0931168ea3d0fe56cb3aedf4463528fa2973c9fd
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2025-03-04 10:37:58 +0100

    Set Language on localStorage

--- Portfolio/src/components/LanguageToggle.tsx
+++ Portfolio/src/components/LanguageToggle.tsx
@@ -7,6 +7,7 @@ const LanguageToggle: React.FC = () => {
 	const toggleLanguage = () => {
 		const newLang = i18n.language === "en" ? "es" : "en";
 		i18n.changeLanguage(newLang);
+		localStorage.setItem("language", newLang);
 	};
 
 	return (
